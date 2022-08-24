a425fd7e806168882fa2c80dfbfc9bb6660deb25
John <mwxfranco@gmail.com>
2022-08-24 18:35:47 +0100

    added inputs and outputs to the nodes, making the width and height of a node dependent on the number of inputs and outputs

--- node_api.py
+++ node_api.py
@@ -1,21 +1,37 @@
 import pygame
 
 pygame.font.init()
-node_name_font = pygame.font.SysFont("Arial", 20)
+node_header_font = pygame.font.SysFont("Consolas", 30)
+node_connection_name_font = pygame.font.SysFont("Consolas", 20)
 
 PYGAME_SCREEN = None
 NODE_LIST = []
 SELECTED_NODE = None
 
+
 class Node:
     def __init__(self, display_name: str):
-        self.rect = None
+        self.rect = pygame.Rect(0, 0, 0, 50)
+        self.xy = None
+        self.custom_wh = None
         self.bg_color = (128, 128, 128)
         self.fg_color = (0, 0, 0)
         self.line_width = 0
         self.border_radius = 5
         self.display_name = display_name
+
         self.mouse_offset = None
+        self.inputs = []
+        self.outputs = []
+
+    def set_xy(self, xy: tuple):
+        self.xy = xy
+        self.rect.x, self.rect.y = xy
+        return self
+
+    def set_wh(self, wh: tuple):
+        self.custom_wh = wh
+        return self
 
     def set_rect(self, xywh: tuple):
         self.rect = pygame.Rect(xywh[0], xywh[1], xywh[2], xywh[3])
@@ -37,6 +53,30 @@ class Node:
         self.border_radius = radius
         return self
 
+    def add_input(self, name: str, color: tuple = (100, 100, 100)):
+        self.inputs.append((name, color))
+        if len(self.inputs) > len(self.outputs):
+            self.rect.height += 50
+        return self
+
+    def remove_input(self, index: int):
+        if len(self.inputs) > len(self.outputs):
+            self.rect.height -= 50
+        self.inputs.pop(index)
+        return self
+
+    def add_output(self, name: str, color: tuple = (100, 100, 100)):
+        self.outputs.append((name, color))
+        if len(self.outputs) > len(self.inputs):
+            self.rect.height += 50
+        return self
+
+    def remove_output(self, index: int):
+        if len(self.outputs) > len(self.inputs):
+            self.rect.height -= 50
+        self.outputs.pop(index)
+        return self
+
     def attach_to_mouse(self):
         if self.mouse_offset:
             return
@@ -46,9 +86,10 @@ class Node:
     def detach_from_mouse(self):
         if self.mouse_offset:
             mouse_pos = pygame.mouse.get_pos()
-            self.rect = pygame.Rect(mouse_pos[0] - self.mouse_offset[0], mouse_pos[1] - self.mouse_offset[1], self.rect[2], self.rect[3])
+            self.rect = pygame.Rect(mouse_pos[0] - self.mouse_offset[0], mouse_pos[1] - self.mouse_offset[1],
+                                    self.rect[2], self.rect[3])
             self.mouse_offset = None
-        
+
     def bring_to_front(self):
         NODE_LIST.append(NODE_LIST.pop(NODE_LIST.index(self)))
 
@@ -59,17 +100,28 @@ class Node:
         render_rect = self.rect
         if self.mouse_offset:
             mouse_pos = pygame.mouse.get_pos()
-            render_rect = pygame.Rect(mouse_pos[0] - self.mouse_offset[0], mouse_pos[1] - self.mouse_offset[1], self.rect[2], self.rect[3])
+            render_rect.x = mouse_pos[0] - self.mouse_offset[0]
+            render_rect.y = mouse_pos[1] - self.mouse_offset[1]
+
+        render_rect.width = node_header_font.size(self.display_name)[0] + 20
         pygame.draw.rect(PYGAME_SCREEN, self.bg_color, render_rect, self.line_width, self.border_radius)
-        display_text_surface = node_name_font.render(self.display_name, False, (0, 0, 0))
+        display_text_surface = node_header_font.render(self.display_name, False, self.fg_color)
         PYGAME_SCREEN.blit(display_text_surface, (render_rect[0] + 10, render_rect[1] + 10))
+
+        for i in range(len(self.inputs)):
+            input_connection = self.inputs[i]
+            pygame.draw.circle(PYGAME_SCREEN, input_connection[1], (render_rect.x, render_rect.y + 50 + 25 + (50 * i)), 5)
+            PYGAME_SCREEN.blit(node_connection_name_font.render(input_connection[0], False, self.fg_color), (render_rect.x + 10, render_rect.y + 50 + 15 + (50 * i)))
+
 
 def init(screen):
     global PYGAME_SCREEN
     PYGAME_SCREEN = screen
 
+
 def create_node(node: Node):
     NODE_LIST.append(node)
+
 
 def find_selected_node():
     global SELECTED_NODE
@@ -85,6 +137,7 @@ def find_selected_node():
         SELECTED_NODE = sorted_nodes[0]
         SELECTED_NODE.bring_to_front()
 
+
 def update(mouse: tuple):
     global SELECTED_NODE
     if mouse[0]:
@@ -95,6 +148,7 @@ def update(mouse: tuple):
         if SELECTED_NODE:
             SELECTED_NODE.detach_from_mouse()
             SELECTED_NODE = None
+
 
 def render_all():
     for node in NODE_LIST:
