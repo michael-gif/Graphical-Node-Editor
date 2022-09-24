55c49f3d29a9a586bcbc12912478c9dcdb8c3441
John <mwxfranco@gmail.com>
2022-09-24 11:19:34 +0100

    added exit window

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import os
 import pygame
 import tkinter as tk
-import ctypes
 import node_api as na
 
 from threading import Thread
 
-user32 = ctypes.windll.user32
-display_res = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-
 root = tk.Tk()
+root.title("Node Editor")
 root.state('zoomed')
-pygame_embed = tk.Frame(root, width=display_res[0], height=display_res[1])
-pygame_embed.pack()
+
+
+def confirm_quit():
+    quit_window = tk.Toplevel(root)
+    quit_window.title("Close Application")
+    w, h = 200, 50
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    quit_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    label = tk.Label(quit_window, text="Are you sure you want to quit?")
+    button = tk.Button(quit_window, text="Quit", command=quit)
+    label.pack()
+    button.pack()
+
+
+root.protocol("WM_DELETE_WINDOW", confirm_quit)
+menubar = tk.Menu(root)
+file_menu = tk.Menu(menubar, tearoff=False)
+menubar.add_command(label="Exit", command=confirm_quit)
+root.config(menu=menubar)
 
 # embed pygame window
+pygame_embed = tk.Frame(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+pygame_embed.pack()
 os.environ['SDL_WINDOWID'] = str(pygame_embed.winfo_id())
 os.environ['SDL_VIDEODRIVER'] = 'windib'
 
-screen = pygame.display.set_mode(display_res)
+screen = pygame.display.set_mode((root.winfo_screenwidth(), root.winfo_screenheight()))
 pygame.display.init()
 pygame.display.update()
 na.init(screen)
