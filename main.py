b1cfe67dcb383809c91729b4465db15b52232935
John <mwxfranco@gmail.com>
2022-09-24 12:31:59 +0100

    added new node window

--- main.py
+++ main.py
@@ -24,11 +24,74 @@ def confirm_quit():
     label.pack()
     button.pack()
 
+def create_new_node():
+    new_node_window = tk.Toplevel(root)
+    new_node_window.title("Create new node")
+    w, h = 370, 370
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    new_node_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    content = tk.Frame(new_node_window, relief='sunken')
+    content.place(x=10, y=10, width=350, height=350)
+
+    name_label = tk.Label(content, text="Display Name", anchor='w')
+    name_label.place(x=0, y=0, height=30)
+    name_entry = tk.Entry(content)
+    name_entry.place(x=125, y=0, width=225, height=25)
+
+    description_label = tk.Label(content, text="Description", anchor='w')
+    description_label.place(x=0, y=30, height=30)
+    description_entry = tk.Entry(content)
+    description_entry.place(x=125, y=30, width=225, height=25)
+
+    def input_select(event):
+        w = event.widget
+        selected_input.delete(0,tk.END)
+        selected_input.insert(0,w.get(int(w.curselection()[0])))
+
+    def output_select(event):
+        w = event.widget
+        selected_output.delete(0,tk.END)
+        selected_output.insert(0,w.get(int(w.curselection()[0])))
+
+    inputs_label = tk.Label(content, text="Inputs")
+    inputs_label.place(x=0, y=65, width=170, height=25)
+    inputs_listbox = tk.Listbox(content)
+    inputs_listbox.bind('<<ListboxSelect>>', input_select)
+    inputs_listbox.place(x=0, y=100, width=170, height=150)
+    selected_input = tk.Entry(content)
+    selected_input.place(x=0, y=255, width=120, height=25)
+    add_input_button = tk.Button(content, text="+")
+    add_input_button.place(x=120, y=255, width=25, height=25)
+    remove_input_button = tk.Button(content, text="-")
+    remove_input_button.place(x=145, y=255, width=25, height=25)
+
+    outputs_label = tk.Label(content, text="Outputs")
+    outputs_label.place(x=180, y=65, width=170, height=25)
+    outputs_listbox = tk.Listbox(content)
+    outputs_listbox.bind('<<ListboxSelect>>', output_select)
+    outputs_listbox.place(x=180, y=100, width=170, height=150)
+    selected_output = tk.Entry(content)
+    selected_output.place(x=180, y=255, width=120, height=25)
+    add_output_button = tk.Button(content, text="+")
+    add_output_button.place(x=300, y=255, width=25, height=25)
+    remove_output_button = tk.Button(content, text="-")
+    remove_output_button.place(x=325, y=255, width=25, height=25)
+
+    for i in range(10):
+        inputs_listbox.insert(tk.END, i)
+    for i in range(10):
+        outputs_listbox.insert(tk.END, i)
+
 
 root.protocol("WM_DELETE_WINDOW", confirm_quit)
 menubar = tk.Menu(root)
 file_menu = tk.Menu(menubar, tearoff=False)
 menubar.add_command(label="Exit", command=confirm_quit)
+menubar.add_command(label="New Node", command=create_new_node)
 root.config(menu=menubar)
 
 # embed pygame window
