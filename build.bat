4150cffd9c8a4df349c9423d9106c0bb40fcc46c
John <mwxfranco@gmail.com>
2022-09-27 12:32:21 +0100

    updated build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ py -3.8 -m PyInstaller --onefile --noconsole --add-data="icon.ico;." --icon="%CD
 
 echo Moving EXE to root folder
 move "%CD%\dist\main.exe" "%CD%"
-ren main.exe "Node Editor.exe"
+ren main.exe "NodeEditor.exe"
 
 echo Cleaning up
 del /f main.spec
